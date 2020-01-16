660e2b6b564656678863014fccc2278a5e33c31d
Dorival <dorival.querino@gmail.com>
2020-01-15 23:59:01 -0300

    Projeto de exemplo para deploy na nuvem da AWS

---
+++ Parte 12 - Cloud e Deploy/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello(){
+		return "Olá Livro Java";
+	}
+}
