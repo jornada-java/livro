2fdb2abc3bc20dc226591e57ff6ef48e11d644c1
Sandro Giacomozzi <sandrogiacom@gmail.com>
2020-04-21 11:28:44 -0300

    Ajuste na Calculadora

--- Parte 09 - Testes/teste-unidade/src/main/java/com/jornadacolaborativa/Calculadora.java
+++ Parte 09 - Testes/teste-unidade/src/main/java/com/jornadacolaborativa/Calculadora.java
@@ -2,24 +2,20 @@ package com.jornadacolaborativa;
 
 public class Calculadora {
 
-    public Double soma(Double valor1, Double valor2) {
-        var resultado = valor1 + valor2;
-        return resultado;
+    public double soma(double valor1, double valor2) {
+        return valor1 + valor2;
     }
 
-    public Double divisao(Double valor1, Double valor2) {
-        var resultado = valor1 / valor2;
-        return resultado;
+    public double divisao(double valor1, double valor2) {
+        return valor1 / valor2;
     }
 
-    public Double subtracao(Double valor1, Double valor2) {
-        var resultado = valor1 - valor2;
-        return resultado;
+    public double subtracao(double valor1, double valor2) {
+        return valor1 - valor2;
     }
 
-    public Double multiplicao(Double valor1, Double valor2) {
-        var resultado = valor1 * valor2;
-        return resultado;
+    public double multiplicao(double valor1, double valor2) {
+        return valor1 * valor2;
     }
 
 }
--- Parte 09 - Testes/teste-unidade/src/test/java/com/jornadacolaborativa/CalculadoraTest.java
+++ Parte 09 - Testes/teste-unidade/src/test/java/com/jornadacolaborativa/CalculadoraTest.java
@@ -10,11 +10,13 @@ class CalculadoraTest {
     public void somaDeDoisPorDoisRetornaQuatro() {
         // Dado que desejo somar dois número
         Calculadora calculadora = new Calculadora();
-        var entrada1 = 2D;
-        var entrada2 = 2D;
-        var resultadoEsperado = 4D;
+        double entrada1 = 2;
+        double entrada2 = 2;
+        double resultadoEsperado = 4;
+
         // Quando informado como parâmetro os números 2 e 2
-        var resultado = calculadora.soma(entrada1, entrada2);
+        double resultado = calculadora.soma(entrada1, entrada2);
+
         // Então a função de soma retorna o número 4
         assertEquals(resultadoEsperado, resultado, "2 + 2 tem que ser igual a 4");
     }
@@ -23,11 +25,13 @@ class CalculadoraTest {
     public void subtracaoDeTresPorDoisRetornaUm() {
         // Dado que desejo subtrair dois número
         Calculadora calculadora = new Calculadora();
-        var entrada1 = 3D;
-        var entrada2 = 2D;
-        var resultadoEsperado = 1D;
+        double entrada1 = 3;
+        double entrada2 = 2;
+        double resultadoEsperado = 1;
+
         // Quando informado como parâmetro os números 3 e 2
-        var resultado = calculadora.subtracao(entrada1, entrada2);
+        double resultado = calculadora.subtracao(entrada1, entrada2);
+
         // Então a função de subtração retorna o número 1
         assertEquals(resultadoEsperado, resultado, "3 - 2 tem que ser igual a 1");
     }
@@ -36,11 +40,13 @@ class CalculadoraTest {
     public void multiplicacaoDeTresPorsDoisRetornaSeis() {
         // Dado que desejo multiplicar dois número
         Calculadora calculadora = new Calculadora();
-        var entrada1 = 3D;
-        var entrada2 = 2D;
-        var resultadoEsperado = 6D;
+        double entrada1 = 3;
+        double entrada2 = 2;
+        double resultadoEsperado = 6;
+
         // Quando informado como parâmetro os números 3 e 2
         var resultado = calculadora.multiplicao(entrada1, entrada2);
+
         // Então a função de multiplicação retorna o número 6
         assertEquals(resultadoEsperado, resultado, "3 * 2 tem que ser igual a 6");
     }
@@ -49,11 +55,13 @@ class CalculadoraTest {
     public void divisaoDeSeisPorDoisRetornaSeis() {
         // Dado que desejo dividir dois número
         Calculadora calculadora = new Calculadora();
-        var entrada1 = 6D;
-        var entrada2 = 2D;
-        var resultadoEsperado = 3D;
+        double entrada1 = 6;
+        double entrada2 = 2;
+        double resultadoEsperado = 3;
+
         // Quando informado como parâmetro os números 6 e 2
         var resultado = calculadora.divisao(entrada1, entrada2);
+        
         // Então a função de divisão retorna o número 3
         assertEquals(resultadoEsperado, resultado, "6 / 2 tem que ser igual a 3");
     }
