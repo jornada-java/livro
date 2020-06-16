e516da44f76992b63d2f51b906e23975281f323d
Sandro Giacomozzi <sandrogiacom@gmail.com>
2020-06-15 22:29:47 -0300

    Parte 04 - Genericos

--- parte-04/07-estrutura-dados/src/main/java/com/jornadajava/GenericosNumero.java
+++ parte-04/07-estrutura-dados/src/main/java/com/jornadajava/GenericosNumero.java
@@ -1,5 +1,28 @@
 package com.jornadajava;
 
-public class GenericosNumero<T extends Number> {
+import java.util.Arrays;
+import java.util.List;
+
+public class GenericosNumero {
+
+    public static void main(String[] args) {
+        List<Integer> lista1 = Arrays.asList(1, 2, 3);
+        System.out.println(soma(lista1));
+
+        List<Double> lista2 = Arrays.asList(1.1, 2.2, 3.3);
+        System.out.println(soma(lista2));
+    }
+
+    //Recebe como parâmetro qualquer lista cujos elementos estendam de Number
+    public static double soma(List<? extends Number> lista) {
+        double soma = 0;
+
+        for (Number numero : lista) {
+            soma += numero.doubleValue();
+        }
+
+        return soma;
+    }
+
 
 }
