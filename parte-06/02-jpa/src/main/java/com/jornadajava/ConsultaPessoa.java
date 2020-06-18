35a921f6f78b65afc7e1985c737254cc0b057b82
Sandro Giacomozzi <sandrogiacom@gmail.com>
2020-06-18 19:11:37 -0300

    Parte 06 - Deixando JPA funcional

--- parte-06/02-jpa/src/main/java/com/jornadajava/ConsultaPessoa.java
+++ parte-06/02-jpa/src/main/java/com/jornadajava/ConsultaPessoa.java
@@ -7,9 +7,19 @@ import javax.persistence.Persistence;
 public class ConsultaPessoa {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence
-                .createEntityManagerFactory(null);
+                .createEntityManagerFactory("jornada");
 
         EntityManager em = factory.createEntityManager();
+
+        var pessoaNova = new Pessoa();
+        pessoaNova.setCpf("111.222.000-24");
+        pessoaNova.setIdade(24);
+        pessoaNova.setNome("Fulano Pessoa");
+
+        em.getTransaction().begin();
+
+        em.persist(pessoaNova); // Cria o registro no banco de dados
+        System.out.println("Pessoa incluida!");
 
         String jpql = "SELECT p FROM Pessoa p WHERE " +
                 "p.nome = :nomePesquisa";
@@ -21,6 +31,7 @@ public class ConsultaPessoa {
             System.out.println("O nome da pessoa é: " + pessoa.getNome());
         }
 
+        em.getTransaction().commit();
         em.close();
         factory.close();
     }
