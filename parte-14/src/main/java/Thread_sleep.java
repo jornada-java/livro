a63dce1fc25677f6a203c98ddd52322fc5b9cb22
anabneri <$git_config_user_email>
2020-07-21 20:24:36 -0300

    feat(parte-14): adicionando o exemplo com sleep()

---
+++ parte-14/src/main/java/Thread_sleep.java
@@ -0,0 +1,13 @@
+public class Thread_sleep {
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Thread rodando" +i);
+            try {
+                Thread.sleep((long) (Math.random() *1000));
+            } catch (InterruptedException e ) {
+                System.err.println("Essa thread foi interrompida");
+            }
+        }
+    }
+}
