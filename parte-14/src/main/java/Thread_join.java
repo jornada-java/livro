196d7cb36774ba82da753774ea982df8c78943d0
anabneri <$git_config_user_email>
2020-07-21 19:37:24 -0300

    feat(parte-14): adicionando exemplo join()

---
+++ parte-14/src/main/java/Thread_join.java
@@ -0,0 +1,18 @@
+public class Thread_join extends Thread{
+
+    public static void main(String[] args) {
+
+        new Thread().run();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <10; i++) {
+            System.out.println("Thread esta rodando" +i);
+        } try {
+            this.join();
+        } catch (InterruptedException e) {
+            System.err.println("Essa thread foi interrompida");
+        }
+    }
+}
